12a8d2b8bcbbb3704e7090f91e7f6d6b472f38a0
Raissa Vasconcelos mendes <rai.celos20@gmail.com>
2022-11-18 21:09:12 -0300

    feature: Realizado requisito 5.

--- src/Archetypes/Necromancer.ts
+++ src/Archetypes/Necromancer.ts
@@ -3,13 +3,19 @@ import { EnergyType } from '../Energy';
 
 export default class Necromancer extends Archetype {
   private _energyType: EnergyType;
+  static _instancia = 0;
 
   constructor(name: string) {
     super(name);
     this._energyType = 'mana';
+    Necromancer._instancia += 1;
   }
 
   get energyType(): EnergyType {
     return this._energyType;
+  }
+
+  static createdArchetypeInstances(): number {
+    return Necromancer._instancia;
   }
 }
--- src/Archetypes/Ranger.ts
+++ src/Archetypes/Ranger.ts
@@ -3,13 +3,19 @@ import { EnergyType } from '../Energy';
 
 export default class Ranger extends Archetype {
   private _energyType: EnergyType;
+  static _instancia = 0;
 
   constructor(name: string) {
     super(name);
     this._energyType = 'stamina';
+    Ranger._instancia += 1;
   }
 
   get energyType(): EnergyType {
     return this._energyType;
+  }
+
+  static createdArchetypeInstances(): number {
+    return this._instancia;
   }
 }
