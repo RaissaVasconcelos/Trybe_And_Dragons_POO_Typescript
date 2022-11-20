57da151c501ba5a854fc6bb4dd6fdd5b114cc9ef
Raissa Vasconcelos mendes <rai.celos20@gmail.com>
2022-11-19 21:38:23 -0300

    config: corrigindo req 6.

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -3,6 +3,10 @@ import Energy from '../Energy';
 export default interface Fighter {
   defense: number;
   energy?: Energy;
+  lifePoints: number;
+  strength: number;
+  attack(enemy: Fighter): void;
+  receiveDamage(attackPoints: number): number
   special?(enemy: Fighter): void;
   levelUp(): void;
 }
